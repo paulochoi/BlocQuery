8bcf98f4efe46b805323094df3991af7be454eb5
Paulo Choi <makwanza@gmail.com>
2015-09-07 09:50:11 -0400

    Add popup for questions

---
+++ BlocQuery/BlocQuery/QuestionPopupViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuestionPopupViewController.h
+//  BlocQuery
+//
+//  Created by Paulo Choi on 9/7/15.
+//  Copyright (c) 2015 Paulo Choi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionPopupViewController : UINavigationController
+
+@end
---
+++ BlocQuery/BlocQuery/QuestionPopupViewController.m
@@ -0,0 +1,37 @@
+//
+//  QuestionPopupViewController.m
+//  BlocQuery
+//
+//  Created by Paulo Choi on 9/7/15.
+//  Copyright (c) 2015 Paulo Choi. All rights reserved.
+//
+
+#import "QuestionPopupViewController.h"
+
+@interface QuestionPopupViewController ()
+
+@end
+
+@implementation QuestionPopupViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
